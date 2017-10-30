d81e54475879b125b447882e44df5d252961a896
Avnit <avnitsharma@rediffmail.com>
2017-10-30 13:45:09 +0530

    Update datayahoo.py

--- datayahoo.py
+++ datayahoo.py
@@ -5,8 +5,7 @@ import pandas_datareader.data as web
 #creating start and end variable. we would need data between these dates
 start = datetime.datetime(2010,1, 1)
 end = datetime.datetime(2015, 8, 22)
-
-
+#metion the symbol of which you want to pull data of
 df = web.DataReader("ICICIBANK.NS", "yahoo", start, end)
 #prints data frame
 print(df)
